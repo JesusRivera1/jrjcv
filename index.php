bedd5e94b0ef2002e96023d36f5a192089b13513
JesusRivera1 <114432335+JesusRivera1@users.noreply.github.com>
2022-10-01 21:52:45 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+
+
+</head>
+<body>
+
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Sobre mí</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="navbar-nav mr-auto" id="navbarNav">
+				<ul class="navbar-nav" >
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#estudios">Estudios</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#portafolio">Portafolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#intereses">Intereses</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contacto">Contacto</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<br>
+
+	<div class="container" style="padding-top: 50px">
+		<div class="row align-items-start">
+			<div class="col">
+				<h1 align="center" class="card-subtitle mb-2 text-muted">Jesús Rivera Jácome</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+
+
+			<div class="col" >
+				<img src="imagenes/im.jpg" class="img-fluid rounded-start" alt="...">
+			</div>
+		</div>
+		<br>
+
+		<div id="estudios" class="container-fluid" style="background-color:#F0FCFF; color:white; padding-top:60px">
+			<div class="row text-center">
+				<h1 class="display-1" style="color:black;">ESTUDIOS</h1>
+
+
+				<div class="container">
+					<div class="row align-items-start">
+						<div class="col">
+
+
+							<table class="table">
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #ABE1EC;" ></th>
+										<th scope="col" style="background-color: #ABE1EC;">Institución educativa</th>
+
+									</tr>
+								</thead>
+								<tbody>
+									<tr align="left">
+										<th scope="row">1</th>
+										<td>Telesecundaria Emperador Cuauhtemoc</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">2</th>
+										<td>Bachillerato Digital 02</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">3</th>
+										<td colspan="2">Universidad Tecnologoica de Huejotzingo</td>
+									</tr>
+								</tbody>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #ABE1EC";></th>
+										<th scope="col" style="background-color: #ABE1EC";>Cursos</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr align="left">
+										<th scope="row">1</th>
+										<td>"Locución comercial" y "Crea tu marca personal" en MVS</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">2</th>
+										<td>Java Scrip</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">3</th>
+										<td colspan="2">CSS</td>
+									</tr>
+
+
+									<tr align="left">
+										<th scope="row">4</th>
+										<td colspan="2">Scrum Fundamentals Certified</td>
+									</tr>
+								</tbody>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #ABE1EC;"></th>
+										<th scope="col" style="background-color: #ABE1EC;">Conocimientos y Habilidades</th>
+									</tr>
+								</thead>
+
+
+								<tbody>
+									<tr align="left">
+										<th scope="row"></th>
+										<td>
+											<ul>
+												<li type="square"> Word</li>
+												<li type="square"> Excel</li>
+												<li type="square"> Power Point</li>
+												<li type="square"> Publisher</li>
+												<br>
+												<li> Locutor</li>
+												<li> Diseñador Gráfico</li>
+												<li> Productor audiovisual</li>
+												<li> Operador continuista en radio</li>
+											</ul>
+										</td>
+									</tr>
+								</tbody>
+
+
+
+
+							</table>
+
+						</div>
+
+						<div class="col">
+							<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" >
+								<div class="carousel-indicators">
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+								</div>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img src="imagenes/1.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="imagenes/2.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="imagenes/4.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+								</div>
+								<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="visually-hidden">Previous</span>
+								</button>
+								<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="visually-hidden">Next</span>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+	<br>
+
+	<div id="portafolio" class="container" style="padding-top: 50px">
+		<div class="row text-center">
+			<h1 class="display-1">PORTAFOLIO</h1>
+			<div class="row align-items-start">
+
+				<div class="col">
+					<div class="card mb-3" style="max-width: 540px;">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<img src="..." class="img-fluid rounded-start" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Primer cuatrimestre</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+					<div class="card mb-3" style="max-width: 540px;">
+						<div class="row g-0">
+							<div class="col-md-4">
+								<img src="imagenes/5.png" class="img-fluid rounded-start" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Segundo cuatrimestre</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<br>
+				<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="imagenes/7.jpg" class="d-block w-100" alt="..." width="640" height="550">
+						</div>
+						<div class="carousel-item">
+							<img src="imagenes/8.jpg" class="d-block w-100" alt="..." width="640" height="550">
+						</div>
+						<div class="carousel-item">
+							<img src="imagenes/9.jpg" class="d-block w-100" alt="..." width="640" height="550">
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</button>
+				</div>
+			</div>
+		</div>
+
+		<br>
+
+		<div id="intereses" class="container-fluid" style="background-color:#F0FCFF; color:black; padding-top: 50px">
+			<div class="row text-center">
+				<h1 class="display-1" style="color:black;">Intereses</h1>
+
+				<div class="row row-cols-1 row-cols-md-3 g-4">
+					<div class="col">
+						<div class="card" >
+							<h1  class="card-title"><span class="material-icons">headphones</span> </h1>
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<h1 class="card-title"><span class="material-icons">pets</span></h1>
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card">
+							<h1 class="card-title"><span class="material-icons">wallpaper</span></h1>
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+
+	<div id="contacto" class="container-fluid" style="background-color:black; color:white;">
+		<div class="row text-center">
+			<h1 class="display-1">Contacto</h1>
+
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-6 text-center">
+				
+				<h3>REDES SOCIALES</h3>
+				<H1>
+					<a href="https://es-la.facebook.com/"><ion-icon name="logo-facebook"></ion-icon></a> 
+					<a href="https://twitter.com/?lang=es"><ion-icon name="logo-instagram"></ion-icon></a>
+					<a href="https://instagram.com/"><ion-icon name="logo-twitter"></ion-icon></a><br>	
+					
+					<br>
+
+				</div>
+				<div class="col-md-6 text-center">
+					
+					<span class="material-icons">location_on </span> San Lorenzo Chiautzingo Puebla <br>
+					<span class="material-icons">phone </span> 2484885151 <br>
+					<span class="material-icons">mail </span> 3521110191@uth.edu.mx 
+					
+					
+				</div>
+			</H1>
+		</div>
+	</div>
+	<br>
+	<p align="center">Derechos Reservados 2022</p>
+
+
+
+
+
+
+
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
