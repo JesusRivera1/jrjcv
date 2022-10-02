e1786e366ee00ae9425e2f38f62d0319e1d1f434
JesusRivera1 <114432335+JesusRivera1@users.noreply.github.com>
2022-10-01 22:10:31 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -43,12 +43,8 @@
 		<div class="row align-items-start">
 			<div class="col">
 				<h1 align="center" class="card-subtitle mb-2 text-muted">Jesús Rivera Jácome</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p style="text-align: justify;"> SOY UNA PERSONA AMABLE E INTELIGENTE QUE BUSCA SUPERARSE EN CADA OBJETIVO QUE REALICE, ADEMÁS DE QUE SOY MUY ORGANIZADO Y ME GUSTA COMUNICAR MIS IDEAS.
+				ME GUSTA TRABAJAR EN EQUIPO Y ADEMÁS ESCUCHAR LAS OPINIONES DE MIS COMPAÑEROS PARA PODER IMPLEMENTAR O MEJORAR EL ÁREA DONDE ME DESARROLLO.</p>
 			</div>
 
 
@@ -201,138 +197,138 @@
 
 	<div id="portafolio" class="container" style="padding-top: 50px">
 		<div class="row text-center">
-			<h1 class="display-1">PORTAFOLIO</h1>
-			<div class="row align-items-start">
+				<h1 class="display-1">PORTAFOLIO</h1>
+  <div class="row align-items-start">
 
-				<div class="col">
-					<div class="card mb-3" style="max-width: 540px;">
-						<div class="row g-0">
-							<div class="col-md-4">
-								<img src="..." class="img-fluid rounded-start" alt="...">
-							</div>
-							<div class="col-md-8">
-								<div class="card-body">
-									<h5 class="card-title">Primer cuatrimestre</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col">
-					<div class="card mb-3" style="max-width: 540px;">
-						<div class="row g-0">
-							<div class="col-md-4">
-								<img src="imagenes/5.png" class="img-fluid rounded-start" alt="...">
-							</div>
-							<div class="col-md-8">
-								<div class="card-body">
-									<h5 class="card-title">Segundo cuatrimestre</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+    <div class="col">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagenes/11.jpeg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Primer cuatrimestre</h5>
+        <p class="card-text"> En este cuatrimestre nosotros creamos una conexión de redes para una empresa que le permitieron tener una organización administrativa y optimización en la infraestructura de Internet.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagenes/5.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Segundo cuatrimestre</h5>
+        <p class="card-text"> En este cuatrimestre nosotros realizamos una aplicación web con el uso de Java para poder reproducir almacenar y organizar música en una base de datos.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
 
-				<br>
-				<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-					<div class="carousel-inner">
-						<div class="carousel-item active">
-							<img src="imagenes/7.jpg" class="d-block w-100" alt="..." width="640" height="550">
-						</div>
-						<div class="carousel-item">
-							<img src="imagenes/8.jpg" class="d-block w-100" alt="..." width="640" height="550">
-						</div>
-						<div class="carousel-item">
-							<img src="imagenes/9.jpg" class="d-block w-100" alt="..." width="640" height="550">
-						</div>
-					</div>
-					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="visually-hidden">Previous</span>
-					</button>
-					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="visually-hidden">Next</span>
-					</button>
-				</div>
-			</div>
-		</div>
+<br>
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagenes/7.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/8.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/9.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div>
 
-		<br>
+<br>
 
-		<div id="intereses" class="container-fluid" style="background-color:#F0FCFF; color:black; padding-top: 50px">
+<div id="intereses" class="container-fluid" style="background-color:#F0FCFF; color:black; padding-top: 50px">
 			<div class="row text-center">
-				<h1 class="display-1" style="color:black;">Intereses</h1>
+				<h1 class="display-1" style="color:black;"> INTERESES</h1>
 
-				<div class="row row-cols-1 row-cols-md-3 g-4">
-					<div class="col">
-						<div class="card" >
-							<h1  class="card-title"><span class="material-icons">headphones</span> </h1>
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col">
-						<div class="card">
-							<h1 class="card-title"><span class="material-icons">pets</span></h1>
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col">
-						<div class="card">
-							<h1 class="card-title"><span class="material-icons">wallpaper</span></h1>
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-							</div>
-						</div>
-					</div>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card" >
+      <h1  class="card-title"><span class="material-icons">headphones</span> </h1>
+      <div class="card-body">
+        <h5 class="card-title">Escuchar música</h5>
+        <p class="card-text"> Me gusta escuchar música o radio para divertirme y informarme</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <h1 class="card-title"><span class="material-icons">pets</span></h1>
+      <div class="card-body">
+        <h5 class="card-title">Animales</h5>
+        <p class="card-text"> Me gusta jugar con mis mascotas y además ayudar a otros animales que lo necesitan.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <h1 class="card-title"><span class="material-icons">wallpaper</span></h1>
+      <div class="card-body">
+        <h5 class="card-title">Edición fotográfica</h5>
+        <p class="card-text">Me gusta diseñar o editar imágenes en mis tiempos libres para practicar el uso de Photoshop y mis habilidades en el diseño digital.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+<br>
+
+			<div id="contacto" class="container-fluid" style="background-color:black; color:white;">
+				<div class="row text-center">
+					<h1 class="display-1"> CONTACTO</h1>
+
 				</div>
-			</div>
-		</div>
-	</div>
-	<br>
-
-	<div id="contacto" class="container-fluid" style="background-color:black; color:white;">
-		<div class="row text-center">
-			<h1 class="display-1">Contacto</h1>
-
-		</div>
-		<br>
-		<div class="row">
-			<div class="col-md-6 text-center">
-				
-				<h3>REDES SOCIALES</h3>
-				<H1>
+				<br>
+				<div class="row">
+					<div class="col-md-6 text-center">
+						
+						<h3>REDES SOCIALES</h3>
+						<H1>
 					<a href="https://es-la.facebook.com/"><ion-icon name="logo-facebook"></ion-icon></a> 
 					<a href="https://twitter.com/?lang=es"><ion-icon name="logo-instagram"></ion-icon></a>
 					<a href="https://instagram.com/"><ion-icon name="logo-twitter"></ion-icon></a><br>	
-					
-					<br>
+					 
+					 <br>
 
-				</div>
-				<div class="col-md-6 text-center">
-					
-					<span class="material-icons">location_on </span> San Lorenzo Chiautzingo Puebla <br>
-					<span class="material-icons">phone </span> 2484885151 <br>
-					<span class="material-icons">mail </span> 3521110191@uth.edu.mx 
-					
-					
-				</div>
-			</H1>
+					</div>
+					<div class="col-md-6 text-center">
+						
+						<span class="material-icons">location_on </span> San Lorenzo Chiautzingo Puebla <br>
+						<span class="material-icons">phone </span> 2484885151 <br>
+						<span class="material-icons">mail </span> 3521110191@uth.edu.mx 
+						
+						
+					</div>
+				</H1>
+			</div>
 		</div>
-	</div>
-	<br>
-	<p align="center">Derechos Reservados 2022</p>
+		<br>
+					<p align="center">Derechos Reservados 2022</p>
 
 
 
